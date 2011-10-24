8cf63acef657e544304aa1e0d36de4edaa54f315
ahes <marcin@rootnode.net>
2011-10-24 23:58:10 +0200

    YAML backup configuration

---
+++ backup/backup.pl
@@ -0,0 +1,140 @@
+#!/usr/bin/perl
+
+# backup script
+# Rootnode http://rootnode.net
+#
+# Copyright (C) 2011 Marcin Hlybin
+# All rights reserved.
+
+use warnings;
+use strict;
+use YAML qw(LoadFile Dump);
+use Data::Dumper;
+
+# config
+my $config_file = 'backup.yaml';
+-f $config_file or die "Config file not found!\n";
+my $yaml = YAML::LoadFile($config_file);
+my $rdiff = '/bin/bash /rootnode/config/backup/rdiff.sh';
+my $backup_dir = '/backup';
+my $remove_older_than = '14D';
+my $hostname = `hostname -s`;
+chomp $hostname;
+
+# interactive mode
+my $debug = ! system('tty -s');
+
+sub check_backup {
+        my($server_name, $user_name) = @_;
+	my $last_backup;
+
+	if(defined $user_name) {
+        	$last_backup = `$rdiff -l -u $user_name $server_name`;
+	} else {
+		$last_backup = `$rdiff -l $server_name`;
+	}
+        
+	chomp $last_backup;
+
+        if($last_backup) {
+                my @current = (localtime(time))[3,4,5];
+                my @backup  = (localtime($last_backup))[3,4,5];
+
+                if(@current ~~ @backup) {
+                        # we have current backup 
+                        return 1;
+                }
+        }
+        return;
+}
+
+sub check_errors {
+	my($error_code,$backup_path) = @_;
+	
+	if($error_code == 256) {
+		# interrupted initial backup
+		rmtree($backup_path);
+	} elsif($error_code) {
+		return $error_code;
+	}
+	
+	return;
+}
+
+# system backups
+foreach my $server_name ( @{ $yaml->{$hostname}->{system} } ) {
+	$debug and print "system backup => $server_name...";
+
+	my $backup_path="$backup_dir/system/$server_name";
+	
+	# check for current backup
+	if(check_backup($server_name)) {
+		$debug and print "current\n";
+		next;
+	}
+	
+	# rdiff-backup
+	system("$rdiff $server_name");
+	$debug and print $? ? "error\n" : "done\n";
+
+	# errors
+	if(check_errors($?,$backup_path)) {
+		print "$server_name (error $?)\n";
+	}
+	
+	# remove old backups
+	if(check_backup($server_name)) {
+		system("$rdiff -r $remove_older_than $server_name");
+	}
+} # system backups
+
+# users backups
+foreach my $server_name ( @{ $yaml->{$hostname}->{users} } ) {
+	$debug and print "users backup => $server_name\n";	
+
+	# db
+	my $dbh = DBI->connect("dbi:mysql:rootnode;mysql_read_default_file=/root/.my.system.cnf",undef,undef,{ RaiseError => 1, AutoCommit => 1 });
+	my $db_backup_users = $dbh->prepare('SELECT login FROM uids WHERE block=0 AND del=0 ORDER BY login');
+	my $db_remove_users = $dbh->prepare('SELECT login FROM uids WHERE del=1 ORDER BY login');
+	
+	# create backup
+	$db_backup_users->execute;
+	while(my($user_name) = $db_backup_users->fetchrow_array) {
+		$debug and print $user_name.'...';
+		
+		my $backup_path="$backup_dir/users/$user_name";
+
+		# check for current backup
+		if(check_backup($server_name, $user_name)) {
+			$debug and print "current\n";
+			next;
+		}
+
+		# rdiff-backup
+		system("$rdiff -u $user_name $server_name");
+		$debug and print $? ? "error\n" : "done\n";
+
+		# errors
+		if(check_errors($?,$backup_path)) {
+			print "$user_name \@$server_name (error $?)\n";
+		}
+	}
+
+	# remove old users
+	$db_remove_users->execute;
+	while(my($user_name) = $db_remove_users->fetchrow_array) {
+		my $backup_path="$backup_dir/users/$user_name";
+		if(-d $backup_path) {
+			rmtree($backup_path);
+		}
+	}
+
+	# remove old backups
+	$db_backup_users->execute;
+	while(my($user_name) = $db_backup_users->fetchrow_array) {
+		if(check_backup($server_name, $user_name)) {
+			system("$rdiff -r $remove_older_than -u $user_name $server_name");
+		}
+	}
+} # users backups
+
--- backup/rdiff.sh
+++ backup/rdiff.sh
@@ -9,25 +9,57 @@
 # exit on error
 set -e 
 
-# show usage
-[ $# -eq 0 ] && echo -e "Usage: $0 server_name [user_name]" && exit 1 
+# usage
+usage() {
+	echo -e "Usage: $0 [ -l ] [ -r time_spec ] [ -u|-m user_name ] server_name" 
+	exit 1	
+}
 
-# args 
-server_name=$1
-user_name=$2
+[ $# -eq 0 ] && usage
 
 # dirs
 home_dir="/home"
 backup_dir="/backup"
 cd $backup_dir
 
-# check backup type
-[ -z $user_name ] && backup_type='system' || backup_type='users';
+# options
+while getopts ":lr:u:m:" opt 
+do
+	case $opt in
+	l) do_listing=1                                     ;;
+	r) do_remove=1         ; remove_older_than=$OPTARG  ;;
+	u) backup_type="users" ; user_name=$OPTARG          ;;
+	m) backup_type="mysql" ; user_name=$OPTARG          ;; 
+	*) usage                                            ;;
+	esac
+done
+
+shift $((OPTIND-1))
+server_name=$1
+backup_type=${backup_type:-system}
+
+case $backup_type in 
+	system)      backup_path="$backup_dir/$backup_type/$server_name" ;;
+	users|mysql) backup_path="$backup_dir/$backup_type/$user_name/$server_name" ;;
+esac
+
+# listing
+if [ $do_listing ] 
+then
+	/usr/bin/rdiff-backup --parsable-output -l $backup_path 2>/dev/null | tail -1 | cut -d' ' -f1
+	exit;
+fi
+
+# remove backup
+if [ $do_remove ] 
+then
+	/usr/bin/rdiff-backup -v0 --remove_older_than $remove_older_than $backup_path
+	exit;
+fi
 
 # set rdiff includes
 case $backup_type in 
 	system ) 
-		destination_dir="$backup_dir/system/$server_name"
 		rdiff_include="
 			--include=/adm \
 			--include=/etc \
@@ -38,13 +70,13 @@ case $backup_type in
 			--include=/var/backups"
 		;;
 	users ) 
-		destination_dir="$backup_dir/users/$user_name/$server_name"
 		rdiff_include="--include=$home_dir/$user_name"
 esac
 
 # create directory
-[ -d $destination_dir ] || mkdir -p -m 700 $destination_dir
+[ -d $backup_path ] || mkdir -p -m 700 $backup_path
 
+# create backup
 /usr/bin/rdiff-backup \
 	$rdiff_include \
 	--exclude=/* \
@@ -53,7 +85,7 @@ esac
 	--exclude-sockets \
 	--exclude-if-present .nobackup \
 	--preserve-numerical-ids \
-root@$server_name.rootnode.net::/ $destination_dir 2>/dev/null
+root@$server_name.rootnode.net::/ $backup_path 2>/dev/null
 
 # client side /root/.ssh/authorized_keys:
 # command="nice-n 19 /usr/bin/rdiff-backup --server --restrict-read-only /",from="IP ADDRESS HERE",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty ssh-rsa SSH_KEY_HERE
